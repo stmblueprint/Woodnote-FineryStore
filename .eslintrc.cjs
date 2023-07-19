970b10d43298a94660ec2662aeaeea1cf288dc8c
stmblueprint <tullochaaron@yahoo.com>
2023-07-19 14:00:02 -0400

    deploy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,7 +8,7 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'node'],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
