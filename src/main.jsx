5ea6dd803bd5bebeac47b9cf27e1fe27118d0d16
stmblueprint <tullochaaron@yahoo.com>
2023-07-19 14:20:34 -0400

    deplo

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
