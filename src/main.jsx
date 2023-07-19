df89eb2be35a5e742bc1322907c835a8bf21c988
stmblueprint <tullochaaron@yahoo.com>
2023-07-19 15:04:31 -0400

    deplo

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from '.'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  //  {/* </React.StrictMode>, */}
 )
