c8a6984926a2b03a19f804616b4f227e05299e6e
stmblueprint <tullochaaron@yahoo.com>
2023-07-19 12:17:19 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -1,12 +1,50 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const http  = require('http')
+const Server  = require("socket.io").Server
+const app = express()
+const path  = require('path')
 
-const app = express();
-const port = process.env.PORT || 5001;
+const server  = http.createServer(app)
+const io = new Server(server , {
+    cors:{
+        origin:"*"
+    }
+})
 
-app.use(express.static(path.join(__dirname, 'client')));
-// app.use(express.static(path.join(__dirname, 'client')));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../client/dist");
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname, "../client/dist/index.html"),
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+      );
+
+})
+
+
+
+
+io.on("connection" , (socket) => {
+   console.log('We are connected')
+
+   socket.on("chat" , chat => {
+      io.emit('chat' , chat)
+   } )
+
+   socket.on('disconnect' , ()=> {
+    console.log('disconnected')
+   })
+})
+
+
+
+server.listen(5001 , () => console.log('Listening to port 5001'))
