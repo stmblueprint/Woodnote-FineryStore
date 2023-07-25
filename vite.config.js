df558e84bd61236db196a956f4c3f02434ea848f
stmblueprint <tullochaaron@yahoo.com>
2023-07-25 12:32:11 -0400

    server env var update

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server:{
-  //   proxy: {
-  //     '/api': 'http://localhost:5001',
-  //   }
-  // },
+  server:{
+    proxy: {
+      '/api': 'https://main.d37zqkts5spj19.amplifyapp.com',
+    }
+  },
   build: {
     outDir: 'dist', // Specify the output directory for the build artifacts
   },
