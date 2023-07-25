a9bc2d1dc4589aaa814735755ee832e4dab2f0b4
stmblueprint <tullochaaron@yahoo.com>
2023-07-25 13:59:17 -0400

    server env var update

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   proxy: {
-    '/api': {
-      target: 'http://localhost:5001', // Replace this with the correct URL of your backend server
+    "/api": {
+      target: "https://main.d37zqkts5spj19.amplifyapp.com", // Replace this with the correct URL of your backend server
       changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, '')
-    }
+      rewrite: (path) => path.replace(/^\/api/, ""),
+    },
   },
   build: {
-    outDir: 'dist', // Specify the output directory for the build artifacts
+    outDir: "dist", // Specify the output directory for the build artifacts
   },
 });
