59a9b97e51056bd50193a39570a386e01ccab25f
stmblueprint <tullochaaron@yahoo.com>
2023-07-28 17:33:48 -0400

    cart items update

--- src/index.jsx
+++ src/index.jsx
@@ -20,7 +20,7 @@ import {
 import { db } from './firebase/firebase';
 
 function App() {
-  const [clientId, setClientId] = useState('');
+  const [clientId, setClientId] = useState(null);
   const [loading, setLoading] = useState(true); // to get clientId before the page loads
 
 
@@ -28,9 +28,18 @@ function App() {
 
 
   useEffect(() => {
-  
+    const storedClientId = sessionStorage.getItem('clientId');
 
-    const fetchClientId = async () => {
+    if (storedClientId) {
+      setClientId(storedClientId);
+      setLoading(false);
+    } else {
+      fetchClientId();
+    }
+  }, []);
+
+  const fetchClientId = async () => {
+    try {
       const qRef = query(collection(db, 'AdminItems'), where('payPalClientId', '!=', ''));
       const querySnapshot = await getDocs(qRef);
 
@@ -39,15 +48,21 @@ function App() {
         const clientId = doc.data().payPalClientId;
 
         setClientId(clientId);
+        sessionStorage.setItem('clientId', clientId);
+      } else {
+        console.error('No PayPal client ID found in Firestore. Please ensure the "payPalClientId" field is set.');
+        // Set a default client ID here if required
+        // setClientId('YOUR_DEFAULT_CLIENT_ID');
       }
-      
 
       setLoading(false);
-    };
-
-    fetchClientId();
-
-  }, []);
+    } catch (error) {
+      console.error('Error fetching PayPal client ID:', error);
+      // Set a default client ID here if required
+      // setClientId('YOUR_DEFAULT_CLIENT_ID');
+      setLoading(false);
+    }
+  };
 
   return (
     <CartProvider>
